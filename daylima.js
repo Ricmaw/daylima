855f26e1532f4990804cce0a6794a18169a4f1b8
Ricmaw <rickydarmawan040901@gmail.com>
2023-12-21 20:55:56 +0700

    first commit

---
+++ daylima.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+// code ini berfungsi untuk berkata hello world
+
+var namaLengkap = "Ricky";
+var alamat = `Palembang`;
+
+console.log(`Nama saya ${namaLengkap} alamat saya di ${alamat}`);
